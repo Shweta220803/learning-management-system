452803d6d5a864dbf6e0476a371b39c49e90f5a8
Shweta220803 <shwetaguraru99@gmail.com>
2025-05-02 06:31:29 +0530

    database updated with mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,28 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB Database Connected");
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1); // Exit process with failure
+//   }
+// };
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 
+//  Connect to the MongoDB database
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Database Connected");
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1); // Exit process with failure
-  }
+  mongoose.connection.on("connected", () => console.log("Database connected"));
+  await mongoose.connect(
+    `${process.env.MONGODB_URI}/learning-management-system`
+  );
 };
 
 export default connectDB;
